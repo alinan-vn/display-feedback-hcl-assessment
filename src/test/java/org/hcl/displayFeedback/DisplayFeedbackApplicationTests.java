15f643e624e1ce6c98fa5185d14deff14c355fc1
Alejandro Linan <jandrolinan@gmail.com>
2021-02-03 16:04:24 -0600

    first init

---
+++ src/test/java/org/hcl/displayFeedback/DisplayFeedbackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hcl.displayFeedback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisplayFeedbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
