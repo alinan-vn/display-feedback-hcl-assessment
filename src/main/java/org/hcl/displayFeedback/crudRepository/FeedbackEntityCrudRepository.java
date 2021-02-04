21520e00c19168afa40470f40636bbb17c546b01
Alejandro Linan <jandrolinan@gmail.com>
2021-02-04 10:53:57 -0600

    added crudRepository data

--- src/main/java/org/hcl/displayFeedback/crudRepository/FeedbackEntityCrudRepository.java
+++ src/main/java/org/hcl/displayFeedback/crudRepository/FeedbackEntityCrudRepository.java
@@ -1,5 +1,8 @@
 package org.hcl.displayFeedback.crudRepository;
 
-public interface FeedbackEntityCrudRepository {
+import org.hcl.displayFeedback.entity.FeedbackEntity;
+import org.springframework.data.repository.CrudRepository;
+
+public interface FeedbackEntityCrudRepository extends CrudRepository<FeedbackEntity, Long>{
 
 }
